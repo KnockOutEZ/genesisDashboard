a986ed43ce7fb10749ba0b05c4a812bfbbd6f1fb
KnockOutEZ <70368615+KnockOutEZ@users.noreply.github.com>
2022-04-21 23:23:55 +0600

    auth ui done

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -8,6 +8,9 @@ export default function Login() {
         <div className="flex content-center items-center justify-center h-full">
           <div className="w-full lg:w-4/12 px-4">
             <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-200 border-0">
+            <div className="rounded-t mb-0 px-6 py-6">
+                
+                </div>
               <div className="flex-auto px-4 lg:px-10 py-10 pt-0">
                 <div className="text-blueGray-400 text-center mb-3 font-bold">
                   <small>Sign in with credentials</small>
