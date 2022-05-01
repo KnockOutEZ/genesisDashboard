53333871e3037398e2acde462663ad0042db1965
KnockOutEZ <70368615+KnockOutEZ@users.noreply.github.com>
2022-05-02 00:33:39 +0600

    delete button commented ready for beta launch

--- src/components/Utils/Form.js
+++ src/components/Utils/Form.js
@@ -62,13 +62,13 @@ export default function Form(props) {
               >
                 Save
               </button>
-              <button
+              {/* <button
                 onClick={props.HandleDeleteFunc}
                 className="bg-red-500 text-white active:bg-lightBlue-600 font-bold uppercase text-xs px-4 py-2 rounded shadow hover:shadow-md outline-none focus:outline-none mr-1 ease-linear transition-all duration-150"
                 type="button"
               >
                 Delete
-              </button>
+              </button> */}
             </div>
           </div>
         </div>
